e35bd9693ad92064d2f5539108f6ff20fd55e023
unJelly02 <32471586+unJelly02@users.noreply.github.com>
2021-01-10 00:34:39 -0600

    First commit

---
+++ header.js
@@ -0,0 +1,66 @@
+console.log("Your header.js file is loaded correctly");
+$("#toggle").on("click", function(){
+    $("#dropDown").slideToggle();
+  });
+  
+
+// let toggled = false;
+// const nav = document.getElementsByClassName('nav')[0];
+// const btn = document.getElementsByClassName('nav-tgl')[0];
+// btn.onclick = function(evt) {
+//   if (!toggled) {
+//     toggled = true;
+//     evt.target.classList.add('toggled');
+//     nav.classList.add('active');
+//   } else {
+//     toggled = false;
+//     evt.target.classList.remove('toggled');
+//     nav.classList.remove('active');
+//   }
+// }
+
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links");
+// const links = document.querySelector(".nav-links li");
+
+// hamburger.addEventListener("click",() =>{
+//   console.log('clicked');
+//   navLinks.classList.toggle("open");
+// })
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+  } else {
+      x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+
+function fade() {
+  $('.preloader').fadeOut("slow");
+  }
+  setTimeout(fade, 3000);
+  
+  $(".searchButton").on("click", function(){
+    $(".searchButton").toggleClass("active");
+    if ($(".searchButton").hasClass("active")) {
+      $(".searchBar").css("height", "100vh");
+      $("#searchForm").css("opacity","1");
+      $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+    }
+    else {
+      $(".searchBar").css('height', "0vh");
+      $("#searchForm").css("opacity","0");
+      $(".searchIcon").removeClass("fa-times").addClass("fa-search");
+    }
+  });
+  
+  $(".toggle").click(function(){
+    $(this).find(".toggleContainer").toggleClass("active");
+    $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+  });
+   
+  
---
+++ index.js
@@ -0,0 +1,47 @@
+
+console.log("Your index.js file is loaded correctly");
+$("#toggle").on("click", function(){
+    $("#dropDown").slideToggle();
+  });
+  
+
+// let toggled = false;
+// const nav = document.getElementsByClassName('nav')[0];
+// const btn = document.getElementsByClassName('nav-tgl')[0];
+// btn.onclick = function(evt) {
+//   if (!toggled) {
+//     toggled = true;
+//     evt.target.classList.add('toggled');
+//     nav.classList.add('active');
+//   } else {
+//     toggled = false;
+//     evt.target.classList.remove('toggled');
+//     nav.classList.remove('active');
+//   }
+// }
+
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links");
+// const links = document.querySelector(".nav-links li");
+
+// hamburger.addEventListener("click",() =>{
+//   console.log('clicked');
+//   navLinks.classList.toggle("open");
+// })
+
+$(function(){
+  $("#header").load("header.html"); 
+  $("#footer").load("footer.html"); 
+});
+
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links");
+// const links = document.querySelectorAll(".nav-links li");
+
+// hamburger.addEventListener("click", () => {
+//   console.log('clicked');
+//   navLinks.classList.toggle("open");
+//   links.forEach(link => {
+//     link.classList.toggle("fade");
+//   });
+// });
