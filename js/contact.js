3a96dc81cc8764766a6d952ee033a8a3e8f85ebe
unJelly02 <32471586+unJelly02@users.noreply.github.com>
2021-05-13 21:58:22 -0500

    Changes to main pages

--- js/contact.js
+++ js/contact.js
@@ -30,3 +30,17 @@ const sr = ScrollReveal({
 sr.reveal('.name',{}); 
 sr.reveal('.subPara',{delay: 200}); 
 sr.reveal('.container',{delay: 400}); 
+
+const copyEmail = ()=>{
+    var copyText = document.getElementById("myInput");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+    
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+    
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+}
